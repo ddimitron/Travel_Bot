aacf35801969ae5333979f43a1375c7b7d15ad95
Alex123123431 <128491235+Alex123123431@users.noreply.github.com>
2024-05-20 20:20:22 +0300

    Add files via upload

---
+++ Хакатон/weather function.py
@@ -0,0 +1,24 @@
+import requests
+import json
+import telebot
+
+#to do:
+#Написать функцию, которая будет выность ошибки о погоде.
+#Вынести URL в отдельную функцию
+@bot.message_handler(func=lambda message: True)
+def get_weather(message):
+    city = message.text #получаем сообщение о городе от пользователя
+
+    url = ('https://api.openweathermap.org/data/2.5/weather?q='+city+'&units=metric&lang=ru&appid=79d1ca96933b0328e1c7e3e7a26cb347')
+    weather_data = requests.get(url).json() #здесь данные с API преобразуются в понятные для нас 
+    wind_speed = round(weather_data['wind']['speed']) #здесь указывается путь для получения данных по скорости ветра
+    temperature = round(weather_data['main']['temp']) #здесь указывается путь для поулчения данных по температуре
+    temperature_feels = round(weather_data['main']['feels_like']) #здесь указывается путь для получения данных по ощущению температуры
+# в функциях ниже выводим сообщения
+    response = 'Сейчас в городе ' + city + ' ' + str(temperature) + ' градусов\n' 
+    response += 'Ощущается как ' + str(temperature_feels) + ' градусов\n'
+    response += 'Скорость ветра ' + str(wind_speed) + ' м/с'
+
+    bot.reply_to(message, response)
+
+bot.polling()
