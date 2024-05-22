f78890d2bc261fb36a7e8a6985614dc7b1d7be16
Данила <mrbloody032@gmail.com>
2024-05-22 19:05:35 +0300

    end 2

--- Хакатон/gpt.py
+++ Хакатон/gpt.py
@@ -1,12 +1,6 @@
 import requests
 from database import *
-
-FOLDER_ID = "b1g23q9ip6n1jnscc7v7"
-GPT_MODEL = "yandexgpt-lite"
-TOKEN = "t1.9euelZrGzc7MjIqQlI-UjIuMk4zIzO3rnpWayI7Gl4rKiprMyouKlpbPj8bl8_ctdE1N-e94MVhI_d3z920iS03573gxWEj9zef1656Vmp7OnY3NlMeUicuQnpPOmpKS7_zF656Vmp7OnY3NlMeUicuQnpPOmpKSveuelZqelMqdipuZyZyNj5WJzZOUmLXehpzRnJCSj4qLmtGLmdKckJKPioua0pKai56bnoue0oye.UzCCuDTBPN0WgND96tvMGe4en53892b5kz2yuioXgwRn9ftiSU1cFMm0b9c7Hw42kh4wmplGUUAlTI18v8aRAw"
-MODEL_TEMPERATURE = "0.6"
-MAX_MODEL_TOKENS = "500"
-
+from config import *
 def promt(message):
     user_id = message.from_user.id
     database = execute_selection_query('''SELECT city FROM database WHERE user_id = ?''', (user_id,))[0]
@@ -14,12 +8,15 @@ def promt(message):
         return f"Скажи адреса Посольств, Аэропортов и Номера экстренных служб в городе {database[0]}."
     elif message.text == "Узнать интересные места": # Впиши сюда то что у тебя будет на кнопке Посмотреть Достопримечательности
         return f"Расскажи о достопримечательностях и впринципе своё мнение о городе {database[0]}. Ограничься 5 Достопримечательностями."
+    else:
+        return f"Расскажи свежие новости в городе {database[0]}"
 
 # TODO GPT запросы для получения информации о городе или важных контактах
 def gpt(message):
     url = f"https://llm.api.cloud.yandex.net/foundationModels/v1/completion"
 
     user_content = promt(message)
+    print(user_content)
 
     headers = {
         'Authorization': f'Bearer {TOKEN}',
