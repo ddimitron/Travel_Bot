b45e45c5c55e3144b8dde1e0bc524594eeacf079
ddimitron <152066520+ddimitron@users.noreply.github.com>
2024-05-23 22:27:57 +0300

    Add files via upload

--- Хакатон/bot.py
+++ Хакатон/bot.py
@@ -5,14 +5,11 @@ from telebot.types import KeyboardButton, ReplyKeyboardMarkup
 from gpt import gpt
 from weather_function import get_weather
 from database import execute_selection_query, execute_query, prepare_database
+from config import ADMINS, keyboard
 
 bot = telebot.TeleBot("7002498917:AAG82cmSQFUN_Y6epWLQkQEXlrlbTuIgHpg")
-ADMIN = []  # список админов, должен быть в config.py
-keyboard = ['Узнать интересные места', 'Узнать экстренные контакты',
-            'Узнать погоду'] # должнo быть в config.py
+#ТГ токен убирать отсюда?
 
-
-prepare_database()
 
 def make_keyboard(items):
     markup = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
@@ -43,10 +40,9 @@ def handle_start(message):
 
 @bot.message_handler(commands=['help'])
 def handle_help(message):
-
     bot.send_message(message.from_user.id,
                      "Привет, я помогу тебе в путешествиях. "
-                     "Для работы нажимай на кнопки снизу\n"
+                     "Для работы нажимай на команду снизу\n"
                      "/choose_city - выбрать город")
 
 
@@ -55,12 +51,14 @@ def choose_city(message):
     bot.send_message(message.from_user.id, "Напиши любой город мира:")
     bot.register_next_step_handler(message, city)
 
+
 def city(message):
     if message.content_type != 'text':
         bot.send_message(message.from_user.id, 'Отправь текстовое сообщение')
         bot.register_next_step_handler(message, choose_city)
     execute_query('''UPDATE database SET city = ? WHERE user_id = ?''', (message.text, message.from_user.id))
-    choose_action(message)
+    bot.register_next_step_handler(message, choose_action)
+
 
 def choose_action(message):
     markup = make_keyboard(keyboard)
@@ -68,35 +66,58 @@ def choose_action(message):
                      reply_markup=markup)
     bot.register_next_step_handler(message, give_info_city)
 
+
 def give_info_city(message):
     if message.text not in keyboard:
         bot.send_message(message.from_user.id,
                          'Выберите действие из предложенных')
         return
+
     if message.text == 'Узнать погоду':
         city = execute_selection_query("SELECT city FROM database WHERE user_id = ?", (message.from_user.id,))[0][0]
         weather = get_weather(city)
         bot.send_message(message.from_user.id, weather)
         choose_action(message)
-    elif message.text == 'Узнать интересные места' or 'Узнать экстренные контакты':
-        status, content = gpt(message)
-        if status:
-            bot.send_message(message.from_user.id, content) # Ответ
-            choose_action(message)
-        else:
-            bot.send_message(message.from_user.id, content) # При ошибке будет выдавать её.
+
+    elif message.text == 'Другой город':
+        bot.register_next_step_handler(message, choose_city)
+
+    status, content = gpt(message)
+    if status:
+        bot.send_message(message.from_user.id, content) # Ответ
+        bot.register_next_step_handler(message, choose_action)
+    else:
+        bot.send_message(message.from_user.id, content) # При ошибке будет выдавать её.
+
+
+@bot.message_handler(commands=["give_feedback"])
+def reception_back_info(message):
+    bot.send_message(message.from_user.id, "Напиши обратную связь:")
+    bot.register_next_step_handler(message, send_back_info)
+
+
+def send_back_info(message):
+    bot.send_message(message.chat.id, "Спасибо за обратную связь")
+    for admin in ADMINS:
+        bot.send_message(admin, f"От юзера {message.from_user.id} "
+                                f"отправлена обратная информация:\n {message.text}")
 
 
 @bot.message_handler(commands=["debug"])
 def send_logs(message):
     user_id = message.chat.id
-    if user_id in ADMIN:
+    if user_id in ADMINS:
         with open("log_file.txt", "rb") as f:
             bot.send_document(message.chat.id, f)
     else:
         bot.send_message(user_id, "У вас недостаточно прав")
 
 
-bot.infinity_polling()
+if __name__ == "__main__":
+    bot.infinity_polling()
+    logging.info("The bot is running")
+    prepare_database()
+
+
 
 
